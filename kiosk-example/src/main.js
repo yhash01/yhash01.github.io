fc01168aac80a65f564516373150db1319d1c3cc
YukioHashimoto <yhash01@gmail.com>
2025-05-02 10:46:41 +0900

    first commit

---
+++ kiosk-example/src/main.js
@@ -0,0 +1,40 @@
+
+function setup() {
+
+  Alpine.store('model', {
+    currentPage: 'home', // 'home', 'service'
+    currentLanguage: 'english',
+    dialNumber: 'erica.talking@ivr.vc',
+    services: [],
+
+    init() {
+      const params = new URLSearchParams(location.search);
+      if (params.has('number')) {
+        this.dialNumber = params.get('number');
+      }
+      this.services = [
+        { url: this.dialNumber, name: 'Loan' },
+        { url: this.dialNumber, name: 'Advice' },
+        { url: this.dialNumber, name: 'Credit' },
+      ];
+    },
+    get page() {
+      return this.currentPage;
+    },
+    set page(nextPage) {
+      this.currentPage = nextPage;
+    },
+    currentLanguage: 'english',
+    languages: ['english', 'norwegian'],
+    get language() {
+      return this.currentLanguage;
+    },
+    set language(current) {
+      this.currentLanguage = current;
+    },
+  });
+
+}
+
+document.addEventListener('alpine:init', setup);
+
