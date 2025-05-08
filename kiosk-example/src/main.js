e29bc70d1c536af71da362012e5b46582ed47786
YukioHashimoto <yhash01@gmail.com>
2025-05-08 13:38:37 +0900

    kiosk2

--- kiosk-example/src/main.js
+++ kiosk-example/src/main.js
@@ -4,7 +4,7 @@ function setup() {
   Alpine.store('model', {
     currentPage: 'home', // 'home', 'service'
     currentLanguage: 'english',
-    dialNumber: 'erica.talking@ivr.vc',
+    dialNumber: 'AAAAerica.talking@ivr.vc',
     services: [],
 
     init() {
