87571915df417e396080ee9f4987f2e547d9d87a
YukioHashimoto <yhash01@gmail.com>
2025-05-21 21:39:55 +0900

    kiosk-hokuto1

---
+++ kiosk-hokuto/src/main.js
@@ -0,0 +1,78 @@
+
+function setup() {
+  Alpine.store('texts', {
+    lang: {
+      japanese: "日本語",
+      english: "English",
+      chinese: "简体中文",
+      korean: "한국어",
+    },
+    welcome: { 
+      japanese: "ホクト産業へようこそ",
+      english: "Welcome to Hokuto Industory",
+      chinese: "欢迎来到 北斗工业",
+      korean: "홋토 산업에 오신 것을 환영합니다",
+    },
+    touchToStart: { 
+      japanese: "ここに触れてください",
+      english: "Please Touch Here",
+      chinese: "触摸这里",
+      korean: "여기를 만져주세요",
+    },
+    selectOption: { 
+      japanese: "本日のご用件について選択してください",
+      english: "What can we help you?",
+      chinese: "请选择您今天的业务",
+      korean: "오늘의 문제에 대해 선택하십시오",
+    },
+    
+  });
+
+
+  Alpine.store('model', {
+    currentPage: 'home', // 'home', 'service'
+    // currentPage: 'service', 
+    currentLanguage: 'japanese',
+    dialNumber: 'AAAAerica.talking@ivr.vc',
+    services: [],
+    callingScheme: 'sip:',
+
+    
+
+    init() {
+      const params = new URLSearchParams(location.search);
+      if (params.has('number')) {
+        this.dialNumber = params.get('number');
+      }
+
+      if (params.has('webexapp')){
+        this.callingScheme = 'webextel:'
+      }
+      
+      this.services = [
+        { tel: "50", url: "sip:50", japanese: '総合受付', english: 'Reception', chinese: '一般接待', korean: '종합 접수', picture: "ico_eringi.png" },
+        { tel: "94", url: "sip:94", japanese: '総務部', english: 'GA Dept.', chinese: '总务部', korean: "총무부", picture: "ico_hokutokun.png" },
+        { tel: "95", url: "sip:95", japanese: '人事部', english: 'Human Resource', chinese: "人事部", korean: "인사부", picture: "ico_maitake.png" },
+      ];
+ 
+    },
+    get page() {
+      return this.currentPage;
+    },
+    set page(nextPage) {
+      this.currentPage = nextPage;
+    },
+    currentLanguage: 'japanese',
+    languages: ['japanese','english', 'chinese', 'korean'],
+    get language() {
+      return this.currentLanguage;
+    },
+    set language(current) {
+      this.currentLanguage = current;
+    },
+  });
+
+}
+
+document.addEventListener('alpine:init', setup);
+
