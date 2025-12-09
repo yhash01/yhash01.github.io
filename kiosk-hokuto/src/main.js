5ebba9294c944c2df2e1125a09cb2a1b66ed6ec5
YukioHashimoto <yhash01@gmail.com>
2025-12-09 09:59:08 +0900

    bbb

--- kiosk-hokuto/src/main.js
+++ kiosk-hokuto/src/main.js
@@ -14,7 +14,7 @@ function setup() {
       korean: "한국어",
     },
     welcome: { 
-      japanese: "myホクト産業へようこそ",
+      japanese: "ホクト産業へようこそ",
       english: "Welcome to Hokuto Industory",
       chinese: "欢迎来到 北斗工业",
       korean: "홋토 산업에 오신 것을 환영합니다",
